0c21fc04350703a54b684396b8d5b6e05e07b4e2
Darren Burns <darren.burns@fanduel.com>
2019-11-01 16:43:41 +0000

    Simplify 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,35 +1,16 @@
 import React from "react"
 
-import DocsLayout from "../components/docs-layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {graphql} from "gatsby"
 
-const NotFoundPage = ({data}) => {
-  const {allMarkdownRemark} = data
-  const docPages = allMarkdownRemark.edges.map(edge => edge.node.frontmatter)
+const NotFoundPage = () => {
   return (
-      <DocsLayout allDocPages={docPages}>
+      <Layout>
         <SEO title="404: Not found"/>
         <h1>NOT FOUND</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </DocsLayout>
+      </Layout>
   )
 }
-
-export const query = graphql`
-query LinksQuery {
-  allMarkdownRemark {
-    edges {
-      node {
-        frontmatter {
-          title
-          section
-          path
-        }
-      }
-    }
-  }
-}
-`
 
 export default NotFoundPage
